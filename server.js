483384a670d8b109c6b5cd69fb99bd940e0e6b4d
JasonCMLo <jason.chun.man.lo@gmail.com>
2022-10-06 19:28:24 -0400

    Change port to work with heroku

--- server.js
+++ server.js
@@ -1,3 +1,15 @@
+/*
+
+File name: Styles.css
+Student name: Jason Lo
+Student ID: 301234232
+Date: October 6th, 2022
+
+*/
+
+// This file creates a server at a specific port, leveraging the app.js file as the callback function (to determine what will be
+// served to the end user
+
 import debug from "debug";
 debug("comp-229");
 import app from "./app/app.js";
@@ -6,4 +18,7 @@ import http from "http";
 
 const server = http.createServer(app);
 
-server.listen(3000);
+const PORT = normalizePort(process.env.PORT || 3000);
+app.set("port", PORT);
+
+server.listen(PORT);
