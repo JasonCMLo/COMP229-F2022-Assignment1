74c9e336a86d357f399e0e491ba4271b400b7776
JasonCMLo <jason.chun.man.lo@gmail.com>
2022-10-06 19:32:06 -0400

    fixing copy/paste error re: port

--- server.js
+++ server.js
@@ -22,3 +22,16 @@ const PORT = normalizePort(process.env.PORT || 3000);
 app.set("port", PORT);
 
 server.listen(PORT);
+
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+  if (isNaN(port)) {
+    return val;
+  }
+
+  if (port >= 0) {
+    return port;
+  }
+
+  return false;
+}
